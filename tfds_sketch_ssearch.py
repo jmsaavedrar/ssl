a356e951c466bf536105cc93a6f57ea18ec53029
jose.saavedra <jose.saavedra>
2023-06-05 16:29:58 -0400

    sketch search

---
+++ tfds_sketch_ssearch.py
@@ -0,0 +1,81 @@
+#import matplotlib.pyplot as plt
+import sys
+import socket
+ip = socket.gethostbyname(socket.gethostname())
+if ip == '192.168.20.62' :
+    sys.path.insert(0,'/home/DIINF/vchang/jsaavedr/Research/git/datasets')
+else :
+    sys.path.insert(0,'/home/jsaavedr/Research/git/datasets')
+import os
+import tfds_qd.tfds_qd as tfds_qd
+import tensorflow as tf
+import numpy as np
+import models.simsiam as simsiam
+import os 
+import configparser
+import skimage.io as io
+import tensorflow_datasets as tfds
+
+def mnist_map_func(image):
+    image = image['image']    
+    image = tf.image.grayscale_to_rgb(image) 
+    return image
+        
+        
+class SSearch():
+    def __init__(self, configfile, model):
+        config = configparser.ConfigParser()
+        config.read(configfile)
+        self.config_model = config[model]
+        self.config_data = config['DATA']    
+        simsiam_model = simsiam.SimSiam(self.config_data, self.config_model)        
+        simsiam_model.load_weights(self.config_model.get('MODEL_NAME'))
+        self.model= simsiam_model.encoder
+                
+
+    def load_data(self):
+        dataset = self.config_data.get('DATASET')
+        #assert (dataset in ['CIFAR', 'MNIST']), 'dataset is not available'
+        ds = tfds.load('tfds_qd')
+        ds_test = ds['train'].shuffle(1024).batch(1000)
+        ds_test = ds_test.map(mnist_map_func)
+        ds_test = ds_test.take(1)
+        for sample in ds_test :
+            self.data = sample.numpy()
+              
+    
+    def compute_features(self):
+        feats = self.model.predict(self.data)        
+        norm = np.linalg.norm(feats, ord = 2, axis = 1, keepdims = True)
+        feats = feats / norm
+        sim = np.matmul(feats, np.transpose(feats))
+        self.sorted_pos = np.argsort(-sim, axis = 1) 
+
+
+    def visualize(self, idx):
+        size = self.config_data.getint('CROP_SIZE')
+        n = 10
+        image = np.ones((size, n*size, 3), dtype = np.uint8)*255
+        i = 0
+        for i , pos in enumerate(self.sorted_pos[idx, :n]) :
+            image[:, i * size:(i + 1) * size, :] = self.data[pos,:,:,:]
+        return image
+       
+    
+    def get_dataset(self):
+        return self.config_data.get('DATASET')     
+
+if __name__ == '__main__' :
+    
+    ssearch = SSearch('config/qd.ini', 'SIMSIAM')
+    ssearch.load_data()
+    ssearch.compute_features()
+    idxs = np.random.randint(1000, size = 10)
+    dataset = ssearch.get_dataset()
+    for idx in idxs :
+        rimage =  ssearch.visualize(idx)
+        fname = 'result_{}_{}.png'.format(dataset, idx)
+        fname = os.path.join('results',fname)
+        io.imsave(fname, rimage)
+        print('result saved at {}'.format(fname))
+    
