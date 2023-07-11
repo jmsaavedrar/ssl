6a880e7d79fd1639888b7e05509b9790b470e03b
jose.saavedra <jose.saavedra>
2023-07-11 12:15:15 -0400

    testing byol

--- models/sketch_byol.py
+++ models/sketch_byol.py
@@ -144,8 +144,7 @@ class SketchBYOL(tf.keras.Model):
                 loss = 0
                 if (step + 1) %  10 == 0 :
                     print('step : {} loss {}'.format(step + 1, loss), flush = True)
-            #pathfile = os.path.join(ckp_dir, '{:03d}.h5'.format(epoch + 1))
-            pathfile = os.path.join(ckp_dir, 'ckp')
+            pathfile = os.path.join(ckp_dir, '{:03d}.h5'.format(epoch + 1))            
             print(pathfile)            
             self.save_weights(pathfile, save_format = 'tf', overwrite = True)
             print('---- epoch : {} ---- (saved)'.format(epoch + 1), flush = True)
