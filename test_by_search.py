d0597d69984d1b50eb91ce62d66f938a0d709489
jose.saavedra <jose.saavedra>
2023-07-26 12:02:47 -0400

    mAP

--- test_by_search.py
+++ test_by_search.py
@@ -73,7 +73,7 @@ class SSearch():
         ds_test = ds['test']
         ds_images = ds_test.map(lambda image : fn(image, self.config_data.getint('CROP_SIZE') ))
         ds_labels = ds_test.map(lambda image_label: map_label_func(image_label))
-        
+        print(ds_labels)
         self.data = ds_images.numpy()
         self.labels = ds_labels.numpy()
 #         ds_test = ds_test.shuffle(1024).batch(self.size)
